1808ad585047911c0c694cd7efac571371520b37
roch smets <roch.smets@gmail.com>
2023-05-16 16:43:02 +0200

    add test run for yao

---
+++ ionBeam/run03/ionBeam-03a.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+import pyphare.pharein as ph #lgtm [py/import-and-import-from]
+from pyphare.pharein import Simulation
+from pyphare.pharein import MaxwellianFluidModel
+from pyphare.pharein import ElectromagDiagnostics, FluidDiagnostics, ParticleDiagnostics
+from pyphare.pharein import ElectronModel
+from pyphare.simulator.simulator import Simulator
+from pyphare.pharein import global_vars as gv
+from pyphare.pharesee.hierarchy import get_times_from_h5
+from tests.diagnostic import all_timestamps
+from pyphare.pharesee.run import Run
+import os
+from pyphare.pharein.global_vars import sim
+
+
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+mpl.use('Agg')
+
+
+
+def config():
+
+    x1_ = 20.0
+    x2_ = 120.0
+
+    Simulation(
+        time_step = 0.001,
+        final_time = 10.,
+        boundary_types = ("periodic", "periodic"),
+        cells = (1000, 400),
+        dl = (0.2, 0.2),
+        refinement_boxes = {},
+        resistivity = 0.001,
+        hyper_resistivity = 0.001,
+        diag_options = {"format" : "phareh5",
+                        "options" : {"dir" : "03a",
+                                     "mode" : "overwrite"}},
+        #restart_options={"dir" : "checks",
+        #                 "mode" : "overwrite",
+        #                 "timestamps" : [10., 20., 30., 40., 50., 60., 70., 80., 90., 100.],
+        #                 #"restart_time":30.},
+        #interp_order = 1,
+    )
+
+    def densityMain(x, y):
+        return 1.0
+
+    def densityBeam(x, y):
+        return np.piecewise(x, [x < x_1, ((x1_ < x) & (x < x2_)), x > x2_], [lambda x:0.0, lambda x:1.0, lambda x:0.0])
+
+    def bx(x, y):
+        return 1.
+
+    def by(x, y):
+        return 0.
+
+    def bz(x, y):
+        return 0.
+
+    def vB(x, y):
+        return np.piecewise(x, [x < x_1, ((x1_ < x) & (x < x2_)), x > x2_], [lambda x:0.0, lambda x:3.0, lambda x:0.0])
+
+    def v0(x, y):
+        return 0.
+
+    def vthMain(x, y):
+        return np.sqrt(1.0)
+
+    def vthBeam(x, y):
+        return np.sqrt(1.0)
+
+    MaxwellianFluidModel(
+        bx=bx,
+        by=by,
+        bz=bz,
+        main={"charge": 1,
+              "mass": 1,
+              "density": densityMain,
+              "vbulkx": v0,
+              "vbulky": v0,
+              "vbulkz": v0,
+              "vthx": vthMain,
+              "vthy": vthMain,
+              "vthz": vthMain,
+              "nbr_part_per_cell": 100,
+              "init": {"seed": 12}},
+        beam={"charge": 1,
+              "mass": 1,
+              "density": densityBeam,
+              "vbulkx": vB,
+              "vbulky": v0,
+              "vbulkz": v0,
+              "vthx": vthBeam,
+              "vthy": vthBeam,
+              "vthz": vthBeam,
+              "nbr_part_per_cell": 100,
+              "init": {"seed": 12}},
+    )
+
+    ElectronModel(closure="isothermal", Te=1.0)
+
+    sim = ph.global_vars.sim
+    start_time = sim.start_time()
+
+    dt = 100.*sim.time_step
+    nt = (sim.final_time-start_time)/dt+1
+    timestamps_fine = start_time+dt*np.arange(nt)
+
+    dt = 1000.*sim.time_step
+    nt = (sim.final_time-start_time)/dt+1
+    timestamps_coarse = start_time+dt*np.arange(nt)
+
+    for quantity in ["E", "B"]:
+        ElectromagDiagnostics(
+            quantity=quantity,
+            write_timestamps=timestamps_fine,
+            compute_timestamps=timestamps_fine,
+        )
+    for quantity in ["density", "bulkVelocity"]:
+             FluidDiagnostics(
+                 quantity=quantity,
+                 write_timestamps=timestamps_fine,
+                 compute_timestamps=timestamps_fine
+             )
+
+    poplist = ["main", "beam"]
+    for pop in poplist:
+        for quantity in ["density", "flux"]:
+            FluidDiagnostics(quantity=quantity,
+                             write_timestamps=timestamps_fine,
+                             compute_timestamps=timestamps_fine,
+                             population_name=pop)
+
+        for quantity in ['domain']: #, 'levelGhost', 'patchGhost']:
+            ParticleDiagnostics(quantity=quantity,
+                                compute_timestamps=timestamps_coarse,
+                                write_timestamps=timestamps_coarse,
+                                population_name=pop)
+
+
+
+
+
+def main():
+
+    config()
+    sim = Simulator(gv.sim)
+    sim.initialize()
+    sim.run()
+
+
+if __name__=="__main__":
+    main()
+
---
+++ ionBeam/run03/job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## ___ sbatch directives
+#SBATCH --job-name=ionBeam-03a
+#SBATCH --output=%x-%j.log
+#
+#SBATCH --ntasks=160
+#SBATCH --time=12:00:00
+#SBATCH --partition=cpu_shared
+#SBATCH --account=phare
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=roch.smets@lpp.polytechnique.fr
+
+## ___ load modules
+module load cmake/3.19.7
+module load gcc/10.2.0
+module load openmpi/4.1.0
+module load hdf5/1.12.1
+module load mambaforge/22.11.1-4
+
+#use conda for phare env... created with mamba !
+conda activate phare
+
+## ___ set the PYTHONPATH
+export PYTHONPATH=/mnt/beegfs/workdir/roch.smets/build-release:/mnt/beegfs/home/LPP/roch.smets/codes/far/PHARE/pyphare
+
+## ___ run phare with python
+mpirun -n $SLURM_NTASKS python ionBeam-03a.py
+
