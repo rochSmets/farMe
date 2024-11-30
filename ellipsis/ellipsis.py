c31dfa3edd65a29d39d65f7ee1271df99c32c304
roch smets <roch.smets@gmail.com>
2024-11-30 23:11:45 +0100

    wip

--- ellipsis/ellipsis.py
+++ ellipsis/ellipsis.py
@@ -14,98 +14,113 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 
+ellipsisCenter = [[40., 0.], [40., 20.]]
+ellipsisAxis = [32., 8.]
+nMain = 0.8
+nBack = 0.2
+widthRatio = 0.3
+Epsilon = 1e-8
+
+
 def slot(x):
     return np.heaviside(x+1, 0.5)-np.heaviside(x-1, 0.5)
 
 
 def polynom(x):
-    return -6*np.abs(x)**5++15*x**4-10*np.abs(x)**3+1
+    return -6*np.abs(x)**5+15*x**4-10*np.abs(x)**3+1
 
 
 def profile(x):
     return slot(x)*polynom(x)
 
 
+def density(x, y):
+    assert x.shape == y.shape
+    # funcs = np.zeros((x.shape[0], x.shape[1], 2))
+    funcs = np.zeros((x.shape[0], 2))
+    for i in range(2):
+        x_ = (x-ellipsisCenter[i][0])/ellipsisAxis[0]
+        y_ = (y-ellipsisCenter[i][1])/ellipsisAxis[1]
+        r_ = np.sqrt(x_**2+y_**2)
+        # funcs[:, :, i] = nMain*profile(r_)
+        funcs[:, i] = nMain*profile(r_)
+    # return funcs.sum(axis=2)
+    return nBack+funcs.sum(axis=1)
+
+
+def bx(x, y):
+    assert x.shape == y.shape
+    # funcs = np.zeros((x.shape[0], x.shape[1], 2))
+    funcs = np.zeros((x.shape[0], 2))
+    for i in range(2):
+        x_ = (x-ellipsisCenter[i][0])/ellipsisAxis[0]
+        y_ = (y-ellipsisCenter[i][1])/ellipsisAxis[1]
+        r_ = np.sqrt(x_**2+y_**2)
+        s_ = (r_-1)/widthRatio
+
+        X_ = +y_/ellipsisAxis[1]
+        Y_ = -x_/ellipsisAxis[0]
+        R_ = np.clip(np.sqrt(X_**2+Y_**2), Epsilon, None)
+
+        # normalisation to ensiure divB == 0
+        Z_ = np.clip(ellipsisAxis[1]*R_/r_, Epsilon, None)
+
+        # funcs[:, :, i] = profile(s_)*Z_*X_/R_
+        funcs[:, i] = profile(s_)*Z_*X_/R_
+    # return funcs.sum(axis=2)
+    return funcs.sum(axis=1)
+
+
+def by(x, y):
+    assert x.shape == y.shape
+    # funcs = np.zeros((x.shape[0], x.shape[1], 2))
+    funcs = np.zeros((x.shape[0], 2))
+    for i in range(2):
+        x_ = (x-ellipsisCenter[i][0])/ellipsisAxis[0]
+        y_ = (y-ellipsisCenter[i][1])/ellipsisAxis[1]
+        r_ = np.sqrt(x_**2+y_**2)
+        s_ = (r_-1)/widthRatio
+
+        X_ = +y_/ellipsisAxis[1]
+        Y_ = -x_/ellipsisAxis[0]
+        R_ = np.clip(np.sqrt(X_**2+Y_**2), Epsilon, None)
+
+        # normalisation to ensiure divB == 0
+        Z_ = np.clip(ellipsisAxis[1]*R_/r_, Epsilon, None)
+
+        # funcs[:, :, i] = profile(s_)*Z_*Y_/R_
+        funcs[:, i] = profile(s_)*Z_*Y_/R_
+    # return funcs.sum(axis=2)
+    return funcs.sum(axis=1)
+
+
+def bz(x, y):
+    return 0.
+
+
+def v0(x, y):
+    return 0.
+
+
+def vth(x, y):
+    return 1.
+
+
 def config():
-    L = 0.5
     Simulation(
-        time_step=0.005,
+        time_step=0.004,
         final_time=.2,
         cells=(400, 100),
-        dl=(0.4, 0.4),
+        dl=(0.2, 0.2),
         hyper_resistivity=0.002,
         resistivity=0.001,
         diag_options={"format": "phareh5",
-                      "options": {"dir": ".",
+                      "options": {"dir": "./ellipsis",
                                   "mode": "overwrite"}, },
-        restart_options={"dir": "checks", "mode": "overwrite",
-                         "timestamps": [10., 20.], }
+        restart_options={"dir": "checks",
+                         "mode": "overwrite",
+                         "timestamps": [.2], }
     )
-
-    x0_ = 0.5
-    y1_ = 0.3
-    y2_ = 0.7
-
-    def density(x, y):
-        from pyphare.pharein.global_vars import sim
-        Ly = sim.simulation_domain()[1]
-        return 0.4 + 1./np.cosh((y-Ly*y1_)/L)**2 + 1./np.cosh((y-Ly*y2_)/L)**2
-
-    def S(y, y0, l):
-        return 0.5*(1. + np.tanh((y-y0)/l))
-
-    def by(x, y):
-        from pyphare.pharein.global_vars import sim
-        Lx = sim.simulation_domain()[0]
-        Ly = sim.simulation_domain()[1]
-        sigma = 1.
-        dB = 0.1
-
-        x0 = (x - x0_ * Lx)
-        y1 = (y - y1_ * Ly)
-        y2 = (y - y2_ * Ly)
-
-        dBy1 = 2*dB*x0*np.exp(-(x0**2+y1**2)/(sigma)**2)
-        dBy2 = -2*dB*x0*np.exp(-(x0**2+y2**2)/(sigma)**2)
-
-        return dBy1 + dBy2
-
-    def bx(x, y):
-        from pyphare.pharein.global_vars import sim
-        Lx = sim.simulation_domain()[0]
-        Ly = sim.simulation_domain()[1]
-        sigma = 1.
-        dB = 0.1
-
-        x0 = (x - x0_ * Lx)
-        y1 = (y - y1_ * Ly)
-        y2 = (y - y2_ * Ly)
-
-        dBx1 = -2*dB*y1 * np.exp(-(x0**2 + y1**2)/(sigma)**2)
-        dBx2 = 2*dB*y2 * np.exp(-(x0**2 + y2**2)/(sigma)**2)
-
-        v1 = -1
-        v2 = 1
-        return v1 + (v2 - v1) * (S(y, Ly * y1_, L) - S(y, Ly * y2_, L))\
-            + dBx1 + dBx2
-
-    def bz(x, y):
-        return 0.
-
-    def b2(x, y):
-        return bx(x, y)**2 + by(x, y)**2 + bz(x, y)**2
-
-    def T(x, y):
-        K = 0.7
-        temp = 1./density(x, y)*(K - b2(x, y)*0.5)
-        assert np.all(temp > 0)
-        return temp
-
-    def v0(x, y):
-        return 0.
-
-    def vth(x, y):
-        return np.sqrt(T(x, y))
 
     MaxwellianFluidModel(
         bx=bx,
@@ -120,40 +135,37 @@ def config():
                  "vthx": vth,
                  "vthy": vth,
                  "vthz": vth,
-                 "nbr_part_per_cell": 500,
+                 "nbr_part_per_cell": 100,
                  "init": {"seed": 12}},
     )
 
-    ElectronModel(closure="isothermal", Te=0.0)
+    ElectronModel(closure="isothermal", Te=0.2)
 
-    sim = ph.global_vars.sim
-    dt = 100.*sim.time_step
-    nt = (sim.final_time)/dt
-    timestamps = dt * np.arange(nt+1)
+    # sim = ph.global_vars.sim
+    # dt = 100.*sim.time_step
+    # nt = (sim.final_time)/dt
+    # timestamps = dt * np.arange(nt+1)
+    timestamps = [0., .1, .2]
 
     for quantity in ["E", "B"]:
         ElectromagDiagnostics(
             quantity=quantity,
             write_timestamps=timestamps,
-            compute_timestamps=timestamps,
         )
 
     for quantity in ["density", "mass_density", "bulkVelocity"]:
         FluidDiagnostics(
             quantity=quantity,
             write_timestamps=timestamps,
-            compute_timestamps=timestamps,
             )
 
     for quantity in ["density", "flux"]:
         FluidDiagnostics(quantity=quantity,
                          write_timestamps=timestamps,
-                         compute_timestamps=timestamps,
                          population_name="protons")
 
     for quantity in ['domain']:  # , 'levelGhost', 'patchGhost']:
         ParticleDiagnostics(quantity=quantity,
-                            compute_timestamps=timestamps,
                             write_timestamps=timestamps,
                             population_name="protons")
 
